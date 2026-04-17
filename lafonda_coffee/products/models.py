86e76d0d40634b0b3fd7a1d3a557a4ce38a5fa43
Juan F gonzalez <juanfelipe22103@gmail.com>
2026-04-17 09:59:28 -0500

    Creacion de la primera app llamada product, se añadio la app a settings.

---
+++ lafonda_coffee/products/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    name = models.TextField(max_length=200, verbose_name='Nombre')
+    descriptioin = models.TextField(max_length=500, verbose_name='Descripcion')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Precio')
+    available = models.BooleanField(default=True, verbose_name='Disponible')
+    photo = models.ImageField(upload_to='logos', null=True, blank=True, verbose_name='Foto')
+    #amount = models.IntegerField(verbose_name='Cantidad')
+
+    def __str__(self):
+        return self.name
+    
